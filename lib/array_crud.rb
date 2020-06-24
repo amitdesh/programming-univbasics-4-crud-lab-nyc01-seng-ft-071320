dfd68be458bde44f0171784fff553d6e95ccbbe7
A D <amitdesh2014@gmail.com>
2020-06-24 16:15:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop
+  return array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array.unshift
-  
+  return array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
